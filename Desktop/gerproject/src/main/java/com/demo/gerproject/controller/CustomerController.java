43bdf2545e96803447e19fcc7e3479f0c15cabf7
cangnss <cgunes52@gmail.com>
2022-12-06 00:50:47 +0300

    update customer process is completed

--- Desktop/gerproject/src/main/java/com/demo/gerproject/controller/CustomerController.java
+++ Desktop/gerproject/src/main/java/com/demo/gerproject/controller/CustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +19,14 @@ public class CustomerController {
 	private static final List<Customer> customers = new ArrayList<>();
 	
 	public CustomerController() {
+		if (customers.isEmpty()) {
+			Customer c1 = new Customer(1, "Ali Can", "Gunes", new Date());
+			Customer c2 = new Customer(2, "Dilek", "Yalcin", new Date());
+			
+			customers.add(c1);
+			customers.add(c2);
+		}
 
-		Customer c1 = new Customer(1, "Ali Can", "Gunes", new Date());
-		Customer c2 = new Customer(2, "Dilek", "Yalcin", new Date());
-
-		customers.add(c1);
-		customers.add(c2);
 	}
 	
 	@GetMapping
@@ -33,12 +36,16 @@ public class CustomerController {
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Customer> getCustomer(@PathVariable int id) {
-		Customer result = customers.stream()
+		Customer result = getCustomerById(id);
+		
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+
+	private Customer getCustomerById(int id) {
+		return customers.stream()
 				 .filter(customer -> customer.getId() == id)
 				 .findFirst()
 				 .orElseThrow(() -> new RuntimeException("Customer is not found!"));
-		
-		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 	
 	@PostMapping
@@ -46,5 +53,14 @@ public class CustomerController {
 		customer.setCreateDate(new Date());
 		customers.add(customer);
 		return new ResponseEntity<>(customer, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> getCustomer(@PathVariable int id, @RequestBody Customer customer){
+		Customer oldCustomer = getCustomerById(id);
+		oldCustomer.setFirstname(customer.getFirstname());
+		oldCustomer.setLastname(customer.getLastname());
+		oldCustomer.setCreateDate(new Date());
+		return new ResponseEntity<>(null, HttpStatus.OK);
 	}
 }
