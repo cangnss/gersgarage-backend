455ecfa1226375739de9bde1d6b28f8abd4c12b1
cangnss <cgunes52@gmail.com>
2022-12-06 12:04:17 +0300

    customer's delete function is added

--- Desktop/gerproject/src/main/java/com/demo/gerproject/controller/CustomerController.java
+++ Desktop/gerproject/src/main/java/com/demo/gerproject/controller/CustomerController.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,6 +62,13 @@ public class CustomerController {
 		oldCustomer.setFirstname(customer.getFirstname());
 		oldCustomer.setLastname(customer.getLastname());
 		oldCustomer.setCreateDate(new Date());
+		return new ResponseEntity<>(null, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteCustomer(@PathVariable int id){
+		Customer founded = getCustomerById(id);
+		customers.remove(founded);
 		return new ResponseEntity<>(null, HttpStatus.OK);
 	}
 }
