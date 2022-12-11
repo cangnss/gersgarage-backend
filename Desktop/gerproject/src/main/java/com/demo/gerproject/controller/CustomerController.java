204777099547cea2118ebe198ff1e07b3725a244
cangnss <cgunes52@gmail.com>
2022-12-11 20:11:41 +0300

    connected database postgresql

--- Desktop/gerproject/src/main/java/com/demo/gerproject/controller/CustomerController.java
+++ Desktop/gerproject/src/main/java/com/demo/gerproject/controller/CustomerController.java
@@ -1,5 +1,9 @@
 package com.demo.gerproject.controller;
 import com.demo.gerproject.model.Customer;
+import com.demo.gerproject.service.CustomerService;
+
+import lombok.AllArgsConstructor;
+
 import java.util.*;
 
 import org.springframework.http.HttpStatus;
@@ -15,45 +19,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/customers")
+@AllArgsConstructor
 public class CustomerController {
 
-	private static final List<Customer> customers = new ArrayList<>();
-	
-	public CustomerController() {
-		if (customers.isEmpty()) {
-			Customer c1 = new Customer(1, "Ali Can", "Gunes", new Date());
-			Customer c2 = new Customer(2, "Dilek", "Yalcin", new Date());
-			
-			customers.add(c1);
-			customers.add(c2);
-		}
-
-	}
+	private final CustomerService customerService;
 	
 	@GetMapping
 	public ResponseEntity<List<Customer>> getCustomers(){
-		return new ResponseEntity<>(customers, HttpStatus.OK);
+		return new ResponseEntity<>(customerService.getCustomers(), HttpStatus.OK);
 	}
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Customer> getCustomer(@PathVariable int id) {
-		Customer result = getCustomerById(id);
-		
-		return new ResponseEntity<>(result, HttpStatus.OK);
+		return new ResponseEntity<>(getCustomerById(id), HttpStatus.OK);
 	}
 
 	private Customer getCustomerById(int id) {
-		return customers.stream()
-				 .filter(customer -> customer.getId() == id)
-				 .findFirst()
-				 .orElseThrow(() -> new RuntimeException("Customer is not found!"));
+		return customerService.getCustomerById(id);
 	}
 	
 	@PostMapping
 	public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer){
-		customer.setCreateDate(new Date());
-		customers.add(customer);
-		return new ResponseEntity<>(customer, HttpStatus.CREATED);
+		return new ResponseEntity<>(customerService.createCustomer(customer), HttpStatus.CREATED);
 	}
 	
 	@PutMapping("/{id}")
@@ -61,14 +48,13 @@ public class CustomerController {
 		Customer oldCustomer = getCustomerById(id);
 		oldCustomer.setFirstname(customer.getFirstname());
 		oldCustomer.setLastname(customer.getLastname());
-		oldCustomer.setCreateDate(new Date());
+		oldCustomer.setCreatedAt(new Date());
 		return new ResponseEntity<>(null, HttpStatus.OK);
 	}
 	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> deleteCustomer(@PathVariable int id){
-		Customer founded = getCustomerById(id);
-		customers.remove(founded);
-		return new ResponseEntity<>(null, HttpStatus.OK);
+		customerService.deleteCustomer(id);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
