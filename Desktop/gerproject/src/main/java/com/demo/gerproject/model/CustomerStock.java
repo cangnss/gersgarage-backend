8e89515c30e5c142e16cc2ba648605ebf95dba08
cangnss <cgunes52@gmail.com>
2022-12-23 23:50:02 +0300

    added stock-customer table

---
+++ Desktop/gerproject/src/main/java/com/demo/gerproject/model/CustomerStock.java
@@ -0,0 +1,19 @@
+package com.demo.gerproject.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerStock {
+
+	@Id
+	private int id;
+	private int customerId;
+	private int stockId;
+}
