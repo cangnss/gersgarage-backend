8ff97461f2e2f7e500fb1db885091d808225cf2b
cangnss <cgunes52@gmail.com>
2023-01-18 21:12:43 +0300

    read data and trained data plotted

--- Desktop/java-svm-project-main/src/LinearSVM.java
+++ Desktop/java-svm-project-main/src/LinearSVM.java
@@ -4,7 +4,9 @@ import weka.core.Instance;
 import weka.core.Instances;
 
 public class LinearSVM {
+	// LinearSVM class for training and predicting data using a linear support vector machine algorithm
 	public static double[] train(Instances data, int[] labels) {
+		// train method for training the data with given labels
 		int n = data.numAttributes();
 		double[] weights = new double[n];
 		Arrays.fill(weights, 0);
@@ -12,6 +14,7 @@ public class LinearSVM {
 		double learningRate = 0.1;
 		int maxIterations = 100;
 
+		// Iterate through the instances and update the weights based on the prediction error
 		for (int iter = 0; iter < maxIterations; iter++) {
 			for (int i = 0; i < data.numInstances(); i++) {
 				Instance instance = data.instance(i);
@@ -27,10 +30,13 @@ public class LinearSVM {
 	}
 
 	public static int predict(Instance data, double[] weights) {
+		// predict method for predicting the label of the given data
 		double prediction = 0;
+		// Calculate the dot product of the instance's attributes and the weights
 		for (int i = 0; i < data.numAttributes(); i++) {
 			prediction += data.value(i) * weights[i];
 		}
+		// Check if the dot product is greater than 0 to predict the label
 		return prediction > 0 ? 1 : -1;
 	}
 }
